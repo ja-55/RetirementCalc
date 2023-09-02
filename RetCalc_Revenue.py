9d1d856c6a74cd4af791a5305d79ee4ab22b0fac
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2023-09-02 11:27:09 -0500

    Edits_090223
    Update date references

--- RetCalc_Revenue.py
+++ RetCalc_Revenue.py
@@ -30,7 +30,7 @@ def gen_fcst_rev(dt_rng, dt_stt, dt_rtr, dt_ssi,
         df_mth_sav_rev.loc[idx,'REV_TOT'] = df_mth_sav_rev.loc[idx,:].sum()
     
     # Timeline - Start + 1Yr ---> Retire
-    for idx, row in df_mth_sav_rev.loc[dt.date(dt_stt.year + 1,1,1):dt_rtr,:].iterrows():
+    for idx, row in df_mth_sav_rev.loc[dt.date(dt_stt.year + 1,1,1):dt.date(dt_rtr.year - 1,12,1),:].iterrows():
         df_mth_sav_rev.loc[idx,'REV_SAL'] = df_mth_sav_rev.shift(12).loc[idx,'REV_SAL'] * (1 + np.random.choice(gr_sal,1,replace = False)[0])
         df_mth_sav_rev.loc[idx,'REV_SSI'] = 0
         df_mth_sav_rev.loc[idx,'REV_GIF'] = rev_gif if idx.month in (2,12) else 0
@@ -41,7 +41,7 @@ def gen_fcst_rev(dt_rng, dt_stt, dt_rtr, dt_ssi,
         df_mth_sav_rev.loc[idx,'REV_TOT'] = df_mth_sav_rev.loc[idx,:].sum()
 
     # Timeline - Retire ---> Retire + 1Yr
-    for idx, row in df_mth_sav_rev.loc[dt.date(dt_rtr.year + 1,1,1):dt.date(dt_rtr.year + 1,12,1),:].iterrows():
+    for idx, row in df_mth_sav_rev.loc[dt_rtr:dt.date(dt_rtr.year,12,1),:].iterrows():
         df_mth_sav_rev.loc[idx,'REV_SAL'] = 0
         df_mth_sav_rev.loc[idx,'REV_SSI'] = 0
         df_mth_sav_rev.loc[idx,'REV_GIF'] = 0
@@ -52,7 +52,7 @@ def gen_fcst_rev(dt_rng, dt_stt, dt_rtr, dt_ssi,
         df_mth_sav_rev.loc[idx,'REV_TOT'] = df_mth_sav_rev.loc[idx,:].sum()
         
     # Timeline - Retire + 1Yr ---> Social Security Eligibility
-    for idx, row in df_mth_sav_rev.loc[dt.date(dt_rtr.year + 2,1,1):dt_ssi,:].iterrows():
+    for idx, row in df_mth_sav_rev.loc[dt.date(dt_rtr.year + 1,1,1):dt.date(dt_ssi.year - 1,12,1),:].iterrows():
         df_mth_sav_rev.loc[idx,'REV_SAL'] = 0
         df_mth_sav_rev.loc[idx,'REV_SSI'] = 0
         df_mth_sav_rev.loc[idx,'REV_GIF'] = 0
@@ -63,7 +63,7 @@ def gen_fcst_rev(dt_rng, dt_stt, dt_rtr, dt_ssi,
         df_mth_sav_rev.loc[idx,'REV_TOT'] = df_mth_sav_rev.loc[idx,:].sum()
         
     # Timeline - Social Security Eligibility ---> Social Security Eligibility + 1Yr
-    for idx, row in df_mth_sav_rev.loc[dt.date(dt_ssi.year + 1,1,1):dt.date(dt_ssi.year + 1,12,1),:].iterrows():
+    for idx, row in df_mth_sav_rev.loc[dt_ssi:dt.date(dt_ssi.year,12,1),:].iterrows():
         df_mth_sav_rev.loc[idx,'REV_SAL'] = 0
         df_mth_sav_rev.loc[idx,'REV_SSI'] = rev_ssi
         df_mth_sav_rev.loc[idx,'REV_GIF'] = 0
@@ -74,7 +74,7 @@ def gen_fcst_rev(dt_rng, dt_stt, dt_rtr, dt_ssi,
         df_mth_sav_rev.loc[idx,'REV_TOT'] = df_mth_sav_rev.loc[idx,:].sum()
 
     # Timeline - Social Security Eligibility + 1Yr ---> End
-    for idx, row in df_mth_sav_rev.loc[dt.date(dt_ssi.year + 2,1,1):,:].iterrows():
+    for idx, row in df_mth_sav_rev.loc[dt.date(dt_ssi.year + 1,1,1):,:].iterrows():
         df_mth_sav_rev.loc[idx,'REV_SAL'] = 0
         df_mth_sav_rev.loc[idx,'REV_SSI'] = df_mth_sav_rev.shift(12).loc[idx,'REV_SSI'] * (1 + np.random.choice(gr_ssi,1,replace = False)[0])
         df_mth_sav_rev.loc[idx,'REV_GIF'] = 0
